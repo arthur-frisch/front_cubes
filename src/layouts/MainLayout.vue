a788c8c3a0e66860b2f548fb29962f02dda8e5cc
arthur-frisch <arthur060704@gmail.com>
2023-03-13 10:01:48 +0100

    feat(home): change background

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,7 +23,13 @@
         <q-btn class="q-mx-sm" icon="logout" dense round flat @click="logOut()"
           ><q-tooltip>Déconnexion</q-tooltip>
         </q-btn>
-        <div>v1.0.0</div>
+        <div class="q-pt-sm">
+          <img
+            alt="Quasar logo"
+            src="../assets/logo_projet.png"
+            style="width: 30px; height: 30px"
+          />
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -38,6 +44,16 @@
         />
       </q-list>
     </q-drawer>
+    <q-page-container style="width: 100%; height: 100%">
+      <q-page class="flex flex-center">
+        <img
+          alt="Quasar logo"
+          src="~assets/accueil_1_neige.png"
+          style="width: 100%; height: 100%"
+        />
+      </q-page>
+    </q-page-container>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -50,10 +66,57 @@
         </q-card-section>
 
         <q-card-section>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
-          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-          minima, porro labore.
+          <q-input v-model="username" label="Nom d'utilisateur"> </q-input>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            v-model="password"
+            :type="isPwd ? 'password' : 'text'"
+            label="Mot de passe"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            v-model="confirmPassword"
+            :type="isConfirmPwd ? 'password' : 'text'"
+            label="Confirmer son mot de passe"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isConfirmPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isConfirmPwd = !isConfirmPwd"
+              />
+            </template>
+          </q-input>
+        </q-card-section>
+
+        <q-separator inset />
+
+        <q-card-section>
+          <span class="text-negative">
+            *Le mot de passe doit contenir au moins 1 chiffre et une majuscule
+          </span>
+        </q-card-section>
+
+        <q-card-section>
+          <q-btn
+            label="Envoyer"
+            type="submit"
+            color="primary"
+            @click="register()"
+            :disable="!isValid"
+          ></q-btn>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -61,7 +124,7 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
 import { useRouter } from "vue-router";
 
@@ -74,11 +137,6 @@ const linksList = [
   },
 ];
 
-function logOut() {
-  localStorage.removeItem("user");
-  $router.push("/login");
-}
-
 export default defineComponent({
   name: "MainLayout",
 
@@ -90,6 +148,21 @@ export default defineComponent({
     const leftDrawerOpen = ref(false);
     const modalAdduser = ref(false);
     const $router = useRouter();
+    const isPwd = ref(true);
+    const isConfirmPwd = ref(true);
+    const username = ref("");
+    const password = ref("");
+    const confirmPassword = ref("");
+    const isValid = computed({
+      get() {
+        return (
+          username.value &&
+          password.value &&
+          confirmPassword.value &&
+          password.value === confirmPassword.value
+        );
+      },
+    });
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
@@ -102,7 +175,18 @@ export default defineComponent({
         $router.push("/login");
       },
       modalAdduser,
+      isPwd,
+      isConfirmPwd,
+      username,
+      password,
+      confirmPassword,
+      isValid,
     };
   },
 });
 </script>
+<style>
+body {
+  overflow: hidden;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,6 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
     meta: { requiredAuth: true },
   },
   {
